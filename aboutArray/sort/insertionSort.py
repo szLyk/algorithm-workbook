02ef3ff9e3d56ff247e930e0b07119d2685c738f
szlyk <349978892@qq.com>
2024-07-17 00:26:46 +0800

    commit

--- aboutArray/sort/insertionSort.py
+++ aboutArray/sort/insertionSort.py
@@ -1,12 +1,12 @@
 # 插入排序：将数组分为已排序和未排序两部分，从未排序部分取出元素插入到已排序部分的正确位置
-one_array = [5, 1, 6, 8, 4, 2, 3, 9, 1]
+one_array = [5, 1, 4, 6, 4, 2, 3, 9, 1]
 
-for i in range(1, len(one_array)):
+for i in range(len(one_array)):
     key = one_array[i]
     j = i - 1
-    while j >= 0 and one_array[j] > key:
-        one_array[j + 1] = one_array[j]
-        j -= 1
-    one_array[j + 1] = key
+    while j >= 0 and key <= one_array[j]:
+        one_array[j + 1] = one_array[j]  # 当前数后移
+        j -= 1  # 往前一个数和现在的数进行比较
+    one_array[j+1] = key
 
 print(one_array)
