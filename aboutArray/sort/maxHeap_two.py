48c4e195184b91eb5816890525e01ae5775d86cd
szlyk <349978892@qq.com>
2024-07-29 23:43:58 +0800

    commit 手动大根堆排序

--- aboutArray/sort/maxHeap_two.py
+++ aboutArray/sort/maxHeap_two.py
@@ -3,26 +3,38 @@
 
 # 每输入一个数 最后结果是一个大根堆
 
-def input_value():
-    some_array = []
+import math
+
+
+def max_heap_insert(heap, value):
+    heap.append(value)
+    heapify_up(heap, len(heap) - 1)
+    return heap
+
+
+def heapify_up(heap, index):
+    parent = (index - 1) // 2
+    while index > 0 and heap[parent] < heap[index]:
+        heap[parent], heap[index] = heap[index], heap[parent]
+        index = parent
+        parent = (index - 1) // 2
+
+
+def main():
+    heap = []
+
     while True:
         try:
-            number = (input("请输入数字: "))
-            if number.__eq__('quit'):
-                break
-            else:
-                some_array.append(int(number))
+            # 接收用户输入
+            value = int(input("Enter a number (or type 'exit' to finish): "))
+            # 将数字加入到数组中
+            heap = max_heap_insert(heap, value)
+            print("Current max heap:", heap)
         except ValueError:
-            print("输入类型错误，退出~~")
+            # 用户输入 'exit'
+            print("Exiting...")
             break
 
 
-def is_odd(number):
-    return number % 2 != 0
-
-
-#  left_child = index * 2 + 1
-#  right_child = index * 2 + 2
-#  parent_index = (left_child - 1) / 2 || parent_index = (left_child - 2) / 2
-def heap_max(some_array, index):
-    largest = index
+if __name__ == "__main__":
+    main()
