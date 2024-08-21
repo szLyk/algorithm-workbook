d8d9339e38e84963364e266d1170ef3e586ff087
yikailuo <yikailuo@zzt.com.cn>
2024-08-21 18:06:41 +0800

    排序复习:二叉树是否为完全二叉树

--- aboutArray/tree/judge_tree.py
+++ aboutArray/tree/judge_tree.py
@@ -1,19 +1,20 @@
 import tree_traversal as tree
 import math
+from collections import deque
 
-ooe_node = tree.TreeNode(4)
-two_node = tree.TreeNode(2)
-three_node = tree.TreeNode(6)
-four_node = tree.TreeNode(1)
-five_node = tree.TreeNode(3)
-six_node = tree.TreeNode(5)
+ooe_node = tree.TreeNode(5)
+two_node = tree.TreeNode(3)
+three_node = tree.TreeNode(7)
+four_node = tree.TreeNode(2)
+five_node = tree.TreeNode(4)
+six_node = tree.TreeNode(6)
 seven_node = tree.TreeNode(7)
 ooe_node.left = two_node
 ooe_node.right = three_node
 two_node.left = four_node
 two_node.right = five_node
 three_node.left = six_node
-three_node.right = seven_node
+four_node.left = seven_node
 
 
 # 判断是否为搜索二叉树
@@ -46,8 +47,6 @@ def check_if_bst(root):
 
 
 # 判断是否为搜索二叉树 用递归的形式
-
-
 def check_if_bst_recursive(node, min_val=-math.inf, max_val=math.inf):
     # 如果当前节点为空，返回True
     if node is None:
@@ -67,4 +66,37 @@ def check_if_bst_recursive(node, min_val=-math.inf, max_val=math.inf):
     return is_left_bst and is_right_bst
 
 
-print(check_if_bst_recursive(ooe_node))
+# 判断是否为完全二叉树 一个方法 两个条件
+# 方法： 需要进行层数遍历
+# 第一个条件： 如果一个根节点只有右节点没有左节点 那就不为二叉树
+# 第二个条件： 在第一个条件满足的条件下 如果是有一个左节点 那么当出现一个没有右节点的节点时 剩下的每一层数据必须是满左右两个节点
+def if_complete_binary_tree(root):
+    if not root:
+        return True
+
+    queue = deque([root])
+    encountered_empty = False  # 标志是否遇到空节点
+
+    while queue:
+        current = queue.popleft()
+
+        # 左子树
+        if current.left:
+            if encountered_empty:
+                return False
+            queue.append(current.left)
+        else:
+            encountered_empty = True
+
+        # 右子树
+        if current.right:
+            if encountered_empty:
+                return False
+            queue.append(current.right)
+        else:
+            encountered_empty = True
+
+    return True
+
+
+print(if_complete_binary_tree(ooe_node))
